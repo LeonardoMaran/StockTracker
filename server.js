a9b7f9bb7efafcacf43f1939d8f88c7b7c62776e
Yuan Zhang <yuan.zhang5@mail.mcgill.ca>
2018-08-17 11:27:49 -0400

    add connection to db

--- server.js
+++ server.js
@@ -6,12 +6,24 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+const stocks = require('./routes/api/stocks');
+
+const db = require('./config/keys').mongoURI;
+
+// connect to DB
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log('Database connected.'))
+    .catch(err => console.log(err));
+
 // Middleware
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.json());
 
 app.use(cors());
+
+app.use('/api/stocks', stocks);
 
 const port = process.env.PORT || 5000;
 
