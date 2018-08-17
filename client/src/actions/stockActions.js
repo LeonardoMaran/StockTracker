1ebd6324c152d393b067793233d4ecf353920407
Yuan Zhang <yuan.zhang5@mail.mcgill.ca>
2018-08-17 12:08:18 -0400

    connect to backend

--- client/src/actions/stockActions.js
+++ client/src/actions/stockActions.js
@@ -1,24 +1,32 @@
 import axios from 'axios';
 import { GET_STOCKS, ADD_STOCK, DELETE_STOCK, STOCKS_LOADING } from './types';
 
-export const getStocks = () => {
-  return {
-    type: GET_STOCKS
-  }
+export const getStocks = () => dispatch => {
+  dispatch(setStocksLoading());
+  axios.get('/api/stocks').then(res =>
+    dispatch({
+      type: GET_STOCKS,
+      payload: res.data
+    })
+  );
 };
 
-export const addStock = (stock) => {
-  return {
-    type: ADD_STOCK,
-    payload: stock
-  }
+export const addStock = (stock) => dispatch => {
+  axios.post('/api/stocks', stock).then(res =>
+    dispatch({
+      type: ADD_STOCK,
+      payload: res.data
+    })
+  );
 };
 
-export const deleteStock = (code) => {
-  return {
-    type: DELETE_STOCK,
-    payload: code
-  }
+export const deleteStock = (_id) => dispatch => {
+  axios.delete(`/api/stocks/${_id}`).then(res =>
+    dispatch({
+      type: DELETE_STOCK,
+      payload: _id
+    })
+  );
 };
 
 export const setStocksLoading = () => {
