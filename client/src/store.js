461c311f3c21ecc3b3ecded85d5b317b2c124d9a
Yuan Zhang <yuan.zhang5@mail.mcgill.ca>
2018-08-17 15:58:56 -0400

    fix redux undefined error

--- client/src/store.js
+++ client/src/store.js
@@ -6,12 +6,13 @@ const initialState = {};
 
 const middleware = [thunk];
 
+const composeEnhancers = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose
+
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(
+    applyMiddleware(...middleware)
   )
 );
 
